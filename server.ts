8d79df6c86ab6db61eae2a0081b7f64389e4f7b5
zhaojunshan3201 <zhaojunshan3201@users.noreply.github.com>
2026-07-16 00:41:37 +0800

    fix: import measure selection scoring helpers

--- server.ts
+++ server.ts
@@ -26,6 +26,7 @@ import {
   type SelectionFilter,
 } from "./src/lib/measureWellSelectionStore.ts";
 import { importMeasureWellWorkbook } from "./src/lib/measureWellImport.ts";
+import { alignOilCurve, evaluateWells } from "./src/lib/measureWellSelection.ts";
 import { buildSelectionCyclesFromTrackingRows } from "./src/lib/measureWellSelectionData.ts";
 import { parseProducingWellsWorkbook, validateWellMapMarkerInput } from "./src/lib/oilWellMap.ts";
 
